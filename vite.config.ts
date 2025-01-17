ac5be7205f4eae128c7fac2ee2ab0cd4ede51b6b
Carlos Maximo <cmaximo@realtec.com.br>
2025-01-17 14:14:52 -0300

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/carlosmaximo/',
   plugins: [react()],
   resolve: {
     alias: {
